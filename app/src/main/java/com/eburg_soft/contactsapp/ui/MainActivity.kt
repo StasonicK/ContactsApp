e7c5cf4e6e6ac1c266ae61019db6dd489890413a
Stas Anishchenko <anischenko_stas@mail.ru>
2020-03-17 20:52:56 +0500

    - Git initialized

---
+++ app/src/main/java/com/eburg_soft/contactsapp/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.eburg_soft.contactsapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eburg_soft.contactsapp.R.layout
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_main)
+    }
+}
