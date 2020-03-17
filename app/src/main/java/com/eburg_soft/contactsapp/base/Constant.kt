dd1d012a38f269e86e0dfc4931a781da6eddbd3e
Stas Anishchenko <anischenko_stas@mail.ru>
2020-03-17 22:05:29 +0500

    - NetworkModule, ApiClient, ContactResponse are ready

--- app/src/main/java/com/eburg_soft/contactsapp/base/Constant.kt
+++ app/src/main/java/com/eburg_soft/contactsapp/base/Constant.kt
@@ -1,2 +1,7 @@
 package com.eburg_soft.contactsapp.base
 
+
+const val BASE_URL_1 = "https://github.com/SkbkonturMobile/mobile-test-droid/blob/master/json/generated-01.json"
+const val BASE_URL_2 = "https://github.com/SkbkonturMobile/mobile-test-droid/blob/master/json/generated-02.json"
+const val BASE_URL_3 = "https://github.com/SkbkonturMobile/mobile-test-droid/blob/master/json/generated-03.json"
+
--- app/src/main/java/com/eburg_soft/contactsapp/model/ContactResponse.kt
+++ app/src/main/java/com/eburg_soft/contactsapp/model/ContactResponse.kt
@@ -1,2 +1,20 @@
 package com.eburg_soft.contactsapp.model
 
+import com.google.gson.annotations.SerializedName
+
+data class Contact (
+
+    @SerializedName("id") val id : String,
+    @SerializedName("name") val name : String,
+    @SerializedName("phone") val phone : String,
+    @SerializedName("height") val height : Double,
+    @SerializedName("biography") val biography : String,
+    @SerializedName("temperament") val temperament : String,
+    @SerializedName("educationPeriod") val contactEducationPeriod : ContactEducationPeriod
+)
+
+data class ContactEducationPeriod (
+
+    @SerializedName("start") val start : String,
+    @SerializedName("end") val end : String
+)
