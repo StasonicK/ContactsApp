84390687e9b6edcc5980d0dc95a68be2e0659669
Stas Anishchenko <anischenko_stas@mail.ru>
2020-03-22 00:08:53 +0500

    - added ContactsListContract, ContactsListPresenter, ContactFragment, BaseFragment;
    - fixed ApiClient, ContactDao;

--- app/src/main/java/com/eburg_soft/contactsapp/presentation/base/BaseFragment.kt
+++ app/src/main/java/com/eburg_soft/contactsapp/presentation/base/BaseFragment.kt
@@ -1,22 +1,4 @@
 package com.eburg_soft.contactsapp.presentation.base
 
-import android.content.Intent
-import android.os.Bundle
-import androidx.annotation.LayoutRes
-import androidx.fragment.app.Fragment
-import com.eburg_soft.contactsapp.R
-
-abstract class BaseFragment(@LayoutRes contentLayoutId: Int) : Fragment(contentLayoutId) {
-
-    //todo set inject
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        //todo set component
-        super.onCreate(savedInstanceState)
-    }
-
-    override fun startActivity(intent: Intent?) {
-        super.startActivity(intent)
-        requireActivity().overridePendingTransition(R.anim.screen_right_in, R.anim.screen_left_out)
-    }
+class BaseFragment {
 }
--- app/src/main/java/com/eburg_soft/contactsapp/presentation/screen/contact/ContactFragment.kt
+++ app/src/main/java/com/eburg_soft/contactsapp/presentation/screen/contact/ContactFragment.kt
@@ -7,18 +7,17 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.eburg_soft.contactsapp.R
-import com.eburg_soft.contactsapp.presentation.base.BaseFragment
 
 /**
  * A simple [Fragment] subclass.
  */
 class ContactFragment : Fragment() {
 
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_contact, container, false)
-//    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contact, container, false)
+    }
 }
