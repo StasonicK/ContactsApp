6fc9b37c92d774857a60ab16397fbd5c59b02af0
Stas Anishchenko <anischenko_stas@mail.ru>
2020-07-31 15:50:58 +0500

    - minor fixes

--- app/src/androidTest/java/com/eburg_soft/contactsapp/fragment/ContactFragmentTest.kt
+++
@@ -1,4 +0,0 @@
-package com.eburg_soft.contactsapp.fragment
-
-class ContactFragmentTest {
-}
--- app/src/androidTest/java/com/eburg_soft/contactsapp/fragment/ContactsListFragmentTest.kt
+++
@@ -1,9 +0,0 @@
-package com.eburg_soft.contactsapp.fragment
-
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.runner.*
-
-@RunWith(AndroidJUnit4::class)
-class ContactsListFragmentTest {
-
-}
