a84f4e3209236950da19983c8c21865844cb4f88
Stas Anishchenko <anischenko_stas@mail.ru>
2020-08-22 11:30:04 +0500

    - changed gitignore file

---
+++ app/src/test/java/com/eburg_soft/contactsapp/GatewayMapperTest.kt
@@ -0,0 +1,25 @@
+package com.eburg_soft.contactsapp
+
+import org.junit.*
+
+class GatewayMapper {
+@Before
+
+
+    @Test
+    fun mappingNullList(){
+
+    }
+
+    @Test
+    fun mappingList(){
+
+    }
+
+//    @Test
+//    fun mappingNullList(){
+//
+//    }
+
+
+}
